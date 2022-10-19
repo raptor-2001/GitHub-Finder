137d43877821768c6233c5445027e3ad2ba77db5
raptor-2001 <pratham007yadav@gmail.com>
2022-10-19 12:27:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/layout/Navbar";
+// eslint-disable-next-line
+import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import PageNotFound from "./pages/PageNotFound";
+import { GithubProvider } from "./context/github/GithubContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+
+function App(){
+  return(
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen">
+          <Navbar title={'GitHub Finder'}/>
+          <main className=" container mx-auto  px-3 p-12">
+            <Routes>
+              <Route path="/" element={<Home/>} />
+              <Route path="/about" element={<About/>} />
+              <Route path="/*" element={<PageNotFound/>} />
+            </Routes>
+          </main>
+          <Footer/>
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
---
+++ src/components/users/UserSearch.jsx
@@ -0,0 +1,66 @@
+import React, { useContext, useState } from 'react'
+import GithubContext from '../../context/github/GithubContext';
+
+
+function UserSearch() {
+
+  const [text, setText] = useState('')
+  const {users, searchUsers, clearUsers} = useContext(GithubContext)
+
+  const handleChange = (e) => setText(e.target.value)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    if(text === ''){
+      alert('Please enter something..')
+    }else{
+
+      searchUsers(text)
+      setText('')
+    }
+  }
+
+  const handleClear = () => {
+    clearUsers()
+  }
+  return (
+    <div 
+      className="grid grid-cols-1xl:grid-cols-2lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+      
+      <div>
+          <form onSubmit={handleSubmit}>
+            <div className="form-control">
+              <div className="relative">
+                <input 
+                  type="text"
+                  className='w-full bg-gray-200 pr-40 input input-lg text-black'
+                  placeholder='Search'
+                  value={text}
+                  onChange={handleChange}
+                />
+                <button 
+                  type='submit'
+                  className="btn btn-lg absolute top-0 right-0 rounded-l-none w-36">
+                  GO
+                </button>
+              </div>
+            </div>
+          </form>
+      </div>
+      
+      {users.length > 0 && (
+        <div>
+          <button 
+            onClick={handleClear}
+            className="btn btn-ghost btn-lg">
+            CLEAR
+          </button>
+        </div>
+      )}
+      
+    </div>
+  )
+}
+
+export default UserSearch
